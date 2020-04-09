5f25490aa00ed3d8baa53cab7389657b0b0d56b3
MQudah <maq2185@gmail.com>
2020-04-10 02:08:02 +0400

    more debug

--- test.py
+++ test.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     moving_angle = 1
     x=[]
     y=[]
-    for i in range(270):
+    for i in range(70):
         #print(dstnc.distance())
         x_y=surround.coordinations(dstnc.distance(),angle)
         x.append(x_y[0])
