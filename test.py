7ee8e6408e5f8d6085b3f1f08f0d4004f859832b
MQudah <maq2185@gmail.com>
2020-04-10 01:30:10 +0400

    debug the log distance issue

--- test.py
+++ test.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     x=[]
     y=[]
     for i in range(180):
-        print(dstnc.distance())
+        #print(dstnc.distance())
         x_y=surround.coordinations(dstnc.distance(),angle)
         x.append(x_y[0])
         y.append(x_y[1])
